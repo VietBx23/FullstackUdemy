dd436a6db76c52cbf7807c2e224eb29aeb5b1d89
VietBx23 <Vietbx23@gmail.com>
2024-11-20 15:32:36 +0700

    connect database

---
+++ src/config/database.js
@@ -0,0 +1,22 @@
+require("dotenv").config(); // Load environment variables from .env file
+const mysql = require("mysql2/promise"); // Import the mysql2/promise module
+
+const createDatabaseConnection = async () => {
+  try {
+    const connection = await mysql.createConnection({
+      host: process.env.DB_HOST, // host name
+      port: process.env.DB_PORT, // default port for mysql
+      user: process.env.DB_USER, // username
+      password: process.env.DB_PASSWORD, // password
+      database: process.env.DB_NAME, // database name
+    });
+    console.log("Database connection established.");
+    return connection;
+  } catch (err) {
+    console.error("Database connection failed:", err.message);
+    throw err;
+  }
+};
+
+// Export một hàm trả về connection
+module.exports = createDatabaseConnection;
--- src/server.js
+++ src/server.js
@@ -1,37 +1,27 @@
 require("dotenv").config();
-const express = require("express"); // commonjs
-const path = require("path");
+const express = require("express");
 const configViewEngine = require("./config/viewEngine");
 const webRouter = require("./routes/web");
-const mysql = require("mysql2/promise"); // dùng `mysql2/promise` để hỗ trợ `async/await`
-const app = express(); // commonjs  app express
-const port = process.env.PORT || 8082; // port number to listen on
+const createDatabaseConnection = require("./config/database");
+
+const app = express();
+const port = process.env.PORT || 8082;
 
 configViewEngine(app);
-
 app.use("/v1", webRouter);
 
-// Hàm kết nối và thực hiện truy vấn MySQL
-const connectToDatabase = async () => {
+const initializeApp = async () => {
   try {
-    const connection = await mysql.createConnection({
-      host: "localhost", // host name
-      port: 3307, // default port for mysql
-      user: "root", // username
-      password: "123456", // password
-      database: "hoidanit", // database name
-    });
-
+    const connection = await createDatabaseConnection();
     const [results, fields] = await connection.query("SELECT * FROM Users u");
     console.log(">>> Results:", results);
     console.log(">>> Fields:", fields);
   } catch (err) {
-    console.error("Database connection failed:", err.message);
+    console.error("Error during app initialization:", err.message);
   }
 };
 
-// Gọi hàm kết nối khi server khởi động
-connectToDatabase();
+initializeApp();
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
